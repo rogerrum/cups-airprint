60a4e1d070de83b3090a688e61a91980620751d3
Roger Rumao <rogerrum@gmail.com>
2021-11-16 16:38:19 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -59,9 +59,6 @@ then
 #  platforms=($(get_available_architectures "$base_image" "$base_tag"))
   platforms=(
     linux/amd64
-    linux/arm/v6
-    linux/arm/v7
-    linux/arm64/v8
   )
 
   BUILD_TYPE=manual
